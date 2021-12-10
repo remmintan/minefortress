8b908dfe23d4a9ec639f99fc1b9eb9bbfeeab5d9
Alexandr <lwjgl@mail.ru>
2021-12-10 15:12:15 +0100

    Initial commit

---
+++ src/main/java/org/minecraftfortress/MineFortressMod.java
@@ -0,0 +1,11 @@
+package org.minecraftfortress;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class MineFortressMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Do nothing
+    }
+}
