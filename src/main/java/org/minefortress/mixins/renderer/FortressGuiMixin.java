6ce7734d5da31445adf4cd9419e864444c4ec845
Alexandr <lwjgl@mail.ru>
2021-12-14 07:42:12 +0100

    Add FortressGui

---
+++ src/main/java/org/minefortress/mixins/renderer/FortressGuiMixin.java
@@ -0,0 +1,52 @@
+package org.minefortress.mixins.renderer;
+
+import com.chocohead.mm.api.ClassTinkerers;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.world.GameMode;
+import org.minefortress.FortressGui;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class FortressGuiMixin extends DrawableHelper {
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+    @Shadow
+    private int scaledWidth;
+    @Shadow
+    private int scaledHeight;
+
+    private FortressGui gui;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    public void init(MinecraftClient client, CallbackInfo ci) {
+        gui = new FortressGui(client);
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V"))
+    public void render(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        final TextRenderer textRenderer = getTextRenderer();
+        gui.render(matrices, textRenderer, scaledWidth, scaledHeight);
+    }
+
+    @Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
+    private void renderCrosshair(MatrixStack matrices, CallbackInfo ci) {
+        if (client.interactionManager != null && client.interactionManager.getCurrentGameMode() == ClassTinkerers.getEnum(GameMode.class, "FORTRESS")) {
+            ci.cancel();
+        }
+    }
+
+    @Shadow
+    public  abstract TextRenderer getTextRenderer();
+
+}
