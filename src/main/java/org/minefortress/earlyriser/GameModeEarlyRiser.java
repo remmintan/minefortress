6be55ca762095da23083ba5dc6426abaa8d0af7b
Alexandr <lwjgl@mail.ru>
2021-12-18 15:27:00 +0100

    Fixed Fabric ASM mapping

--- src/main/java/org/minefortress/earlyriser/GameModeEarlyRiser.java
+++ src/main/java/org/minefortress/earlyriser/GameModeEarlyRiser.java
@@ -1,12 +1,18 @@
 package org.minefortress.earlyriser;
 
 import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.MappingResolver;
 
 public class GameModeEarlyRiser implements Runnable{
     @Override
     public void run() {
+        final MappingResolver mappingResolver = FabricLoader.getInstance().getMappingResolver();
+
+        final String gameMode = mappingResolver.mapClassName("intermediary", "net.minecraft.class_1934");
+
         ClassTinkerers
-                .enumBuilder("net.minecraft.world.GameMode", int.class, String.class)
+                .enumBuilder(gameMode, int.class, String.class)
                 .addEnum("FORTRESS", 4, "fortress")
                 .build();
     }
