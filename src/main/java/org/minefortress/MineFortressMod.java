bc3f084ab63423641c018c5d5caedc59ba141205
Alexandr <lwjgl@mail.ru>
2021-12-19 17:00:35 +0100

    Set scaffold flameable

--- src/main/java/org/minefortress/MineFortressMod.java
+++ src/main/java/org/minefortress/MineFortressMod.java
@@ -3,6 +3,8 @@ package org.minefortress;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.minefortress.entity.Colonist;
@@ -17,10 +19,15 @@ import org.minefortress.registries.FortressItems;
 public class MineFortressMod implements ModInitializer {
     @Override
     public void onInitialize() {
+        // blocks
         Registry.register(Registry.BLOCK, new Identifier("minefortress", "scaffold_oak_planks"), FortressBlocks.SCAFFOLD_OAK_PLANKS);
+        FlammableBlockRegistry.getInstance(Blocks.FIRE).add(FortressBlocks.SCAFFOLD_OAK_PLANKS, 5, 20);
+
+        // entities
         FabricDefaultAttributeRegistry.register(FortressEntities.COLONIST_ENTITY_TYPE, Colonist.createAttributes());
         Registry.register(Registry.ITEM, new Identifier("minefortress", "colonist_spawn_egg"), FortressItems.COLONIST_SPAWN_EGG);
 
+        // networking
         FortressServerNetworkHelper.registerReceiver(FortressChannelNames.NEW_TASK, ServerboundColonistTaskPacket::new);
         FortressServerNetworkHelper.registerReceiver(FortressChannelNames.CANCEL_TASK, ServerboundCancelTaskPacket::new);
     }
