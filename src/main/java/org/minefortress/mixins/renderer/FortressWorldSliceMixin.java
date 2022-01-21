09f1d81e454225eb7df1366fe62861d7986c9100
Alexandr <lwjgl@mail.ru>
2022-01-21 19:16:41 +0100

    Add campfire render to the chunk

--- src/main/java/org/minefortress/mixins/renderer/FortressWorldSliceMixin.java
+++ src/main/java/org/minefortress/mixins/renderer/FortressWorldSliceMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkSectionPos;
 import org.minefortress.blueprints.BlueprintManager;
+import org.minefortress.fortress.FortressClientManager;
 import org.minefortress.interfaces.FortressMinecraftClient;
 import org.minefortress.selections.ClickType;
 import org.minefortress.selections.SelectionManager;
@@ -25,9 +26,32 @@ public abstract class FortressWorldSliceMixin {
     @Inject(method = "unpackBlockData", at = @At("TAIL"))
     public void unpackBlockData(BlockState[] states, ClonedChunkSection section, BlockBox box, CallbackInfo ci) {
         final FortressMinecraftClient fortressClient = (FortressMinecraftClient) MinecraftClient.getInstance();
+
+        final FortressClientManager fortressClientManager = fortressClient.getFortressClientManager();
+        if(fortressClientManager.isFortressInitializationNeeded()) {
+            renderCampPositionOnChunck(states, section, fortressClientManager);
+            return;
+        }
+
         final BlueprintManager blueprintManager = fortressClient.getBlueprintManager();
         if (!blueprintManager.hasSelectedBlueprint()) {
             addSelectionToChunk(states, section, fortressClient);
+        }
+    }
+
+    private void renderCampPositionOnChunck(BlockState[] states, ClonedChunkSection section, FortressClientManager fortressClientManager) {
+        final BlockPos posAppropriateForCenter = fortressClientManager.getPosAppropriateForCenter();
+        if(posAppropriateForCenter != null) {
+            final ChunkSectionPos sectionPos = section.getPosition();
+            BlockPos startPos = new BlockPos(sectionPos.getMinX(), sectionPos.getMinY(), sectionPos.getMinZ());
+            BlockPos endPos = new BlockPos(sectionPos.getMaxX(), sectionPos.getMaxY(), sectionPos.getMaxZ());
+
+            for (BlockPos blockPos : BlockPos.iterate(startPos, endPos)) {
+                if (blockPos.equals(posAppropriateForCenter)) {
+                    final int index = WorldSlice.getLocalBlockIndex(blockPos.getX()&15, blockPos.getY()&15, blockPos.getZ()&15);
+                    states[index] = fortressClientManager.getStateForCampCenter();
+                }
+            }
         }
     }
 
