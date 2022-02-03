463466552e3852e516b9d07b0127296fdeb87e1f
Alexandr <lwjgl@mail.ru>
2022-02-03 17:04:56 +0100

    Fix for the issue

--- src/main/java/org/minefortress/mixins/network/FortressClientPlayNetworkHandlerMixin.java
+++ src/main/java/org/minefortress/mixins/network/FortressClientPlayNetworkHandlerMixin.java
@@ -1,7 +1,10 @@
 package org.minefortress.mixins.network;
 
+import com.chocohead.mm.api.ClassTinkerers;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.util.telemetry.TelemetrySender;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import net.minecraft.world.GameMode;
 import org.minefortress.blueprints.world.BlueprintsWorld;
@@ -17,10 +20,22 @@ public class FortressClientPlayNetworkHandlerMixin {
 
     @Shadow @Final private MinecraftClient client;
 
+    @Shadow @Final private TelemetrySender telemetrySender;
+    private static final GameMode FORTRESS = ClassTinkerers.getEnum(GameMode.class, "FORTRESS");
+
     @Inject(method = "onPlayerRespawn", at = @At("TAIL"))
     public void onPlayerRespawn(PlayerRespawnS2CPacket packet, CallbackInfo ci) {
-        if(packet.getDimension() == BlueprintsWorld.BLUEPRINTS_WORLD_REGISTRY_KEY && this.client.interactionManager != null)
+        if (packet.getDimension() == BlueprintsWorld.BLUEPRINTS_WORLD_REGISTRY_KEY && this.client.interactionManager != null)
             this.client.interactionManager.setGameMode(GameMode.CREATIVE);
+    }
+
+    @Inject(method = "onGameJoin", at = @At(value = "TAIL", shift = At.Shift.BY, by = -1), cancellable = true)
+    public void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
+        final GameMode gameMode = packet.gameMode();
+        if(gameMode == FORTRESS) {
+            this.telemetrySender.setGameModeAndSend(GameMode.CREATIVE, false);
+            ci.cancel();
+        }
     }
 
 }
