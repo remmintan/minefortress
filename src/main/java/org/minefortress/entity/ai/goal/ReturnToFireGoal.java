b3c2b262255290d737f5617e99f7fbb4b1a2d36e
Alexandr <lwjgl@mail.ru>
2022-02-25 08:10:11 +0100

    Pawn won't step in campfire anymore

--- src/main/java/org/minefortress/entity/ai/goal/ReturnToFireGoal.java
+++ src/main/java/org/minefortress/entity/ai/goal/ReturnToFireGoal.java
@@ -3,19 +3,18 @@ package org.minefortress.entity.ai.goal;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.Path;
-import net.minecraft.entity.ai.pathing.PathNodeMaker;
 import net.minecraft.util.math.BlockPos;
 import org.minefortress.entity.Colonist;
 import org.minefortress.entity.ai.NodeMaker;
 
 import java.util.EnumSet;
-import java.util.Iterator;
 import java.util.Random;
 
 public class ReturnToFireGoal extends Goal {
 
     private final Random random = new Random();
-    private static final int HOME_RADIUS = 4;
+    private static final int HOME_INNER_RADIUS = 2;
+    private static final int HOME_OUTER_RADIUS = 4;
     private final Colonist colonist;
 
     public ReturnToFireGoal(Colonist colonist) {
@@ -28,18 +27,18 @@ public class ReturnToFireGoal extends Goal {
     public boolean canStart() {
         final BlockPos fortressCenter = colonist.getFortressCenter();
         return fortressCenter != null &&
-                colonist.squaredDistanceTo(fortressCenter.getX(), fortressCenter.getY(), fortressCenter.getZ()) > Math.pow(HOME_RADIUS, 2);
+                colonist.squaredDistanceTo(fortressCenter.getX(), fortressCenter.getY(), fortressCenter.getZ()) > Math.pow(HOME_OUTER_RADIUS, 2);
     }
 
     @Override
     public void start() {
         super.start();
         final BlockPos fortressCenter = colonist.getFortressCenter().toImmutable();
-        final Iterator<BlockPos> iterator = BlockPos.iterateRandomly(random, 2, fortressCenter, HOME_RADIUS - 1).iterator();
-        BlockPos goal = iterator.next();
-        if(goal.equals(fortressCenter)) {
-            goal = iterator.next();
-        }
+
+        final int x = random.nextInt(HOME_OUTER_RADIUS - HOME_INNER_RADIUS) + HOME_INNER_RADIUS * (random.nextBoolean()?1:-1);
+        final int z = random.nextInt(HOME_OUTER_RADIUS - HOME_INNER_RADIUS) + HOME_INNER_RADIUS * (random.nextBoolean()?1:-1);
+
+        BlockPos goal = new BlockPos(fortressCenter.getX() + x, fortressCenter.getY(), fortressCenter.getZ() + z);
 
         final EntityNavigation navigation = colonist.getNavigation();
         final NodeMaker nodeMaker = (NodeMaker)navigation.getNodeMaker();
