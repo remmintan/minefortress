005ca9a507f6e9ec7c90747f2482e365eea28353
Alexandr <lwjgl@mail.ru>
2022-03-01 07:54:59 +0100

    Update blueprints floor levels

--- src/main/java/org/minefortress/blueprints/manager/ServerBlueprintManager.java
+++ src/main/java/org/minefortress/blueprints/manager/ServerBlueprintManager.java
@@ -39,19 +39,19 @@ public class ServerBlueprintManager {
                             //small_house_4.nbt
                             new BlueprintMetadata("Small House 4", "small_house_4"),
                             //small_house_5.nbt
-                            new BlueprintMetadata("Small House 5", "small_house_5"),
+                            new BlueprintMetadata("Small House 5", "small_house_5", 1),
                             //small_house_6.nbt
                             new BlueprintMetadata("Small House 6", "small_house_6"),
                             //small_house_7.nbt
                             new BlueprintMetadata("Small House 7", "small_house_7"),
                             //small_house_8.nbt
-                            new BlueprintMetadata("Small House 8", "small_house_8"),
+                            new BlueprintMetadata("Small House 8", "small_house_8", 2),
                             //medium_house_1.nbt
-                            new BlueprintMetadata("Medium House 1", "medium_house_1"),
+                            new BlueprintMetadata("Medium House 1", "medium_house_1", 1),
                             //medium_house_2.nbt
                             new BlueprintMetadata("Medium House 2", "medium_house_2"),
                             //big_house_1.nbt
-                            new BlueprintMetadata("Big House 1", "big_house_1")
+                            new BlueprintMetadata("Big House 1", "big_house_1", 1)
                     )
             ),
             entry(
@@ -62,17 +62,17 @@ public class ServerBlueprintManager {
                             //butcher_shop_1.nbt
                             new BlueprintMetadata("Butcher Shop 1", "butcher_shop_1"),
                             //butcher_shop_2.nbt
-                            new BlueprintMetadata("Butcher Shop 2", "butcher_shop_2"),
+                            new BlueprintMetadata("Butcher Shop 2", "butcher_shop_2", 1),
                             //cartographer_1.nbt
-                            new BlueprintMetadata("Cartographer 1", "cartographer_1"),
+                            new BlueprintMetadata("Cartographer 1", "cartographer_1", 1),
                             //fisher_cottage_1.nbt
-                            new BlueprintMetadata("Fisher Cottage 1", "fisher_cottage_1"),
+                            new BlueprintMetadata("Fisher Cottage 1", "fisher_cottage_1", 2),
                             //fletcher_house_1.nbt
-                            new BlueprintMetadata("Fletcher House 1", "fletcher_house_1"),
+                            new BlueprintMetadata("Fletcher House 1", "fletcher_house_1", 1),
                             //masons_house_1.nbt
                             new BlueprintMetadata("Masons House 1", "masons_house_1"),
                             //shepherds_house_1.nbt
-                            new BlueprintMetadata("Shepherds House 1", "shepherds_house_1"),
+                            new BlueprintMetadata("Shepherds House 1", "shepherds_house_1", 1),
                             //tannery_1.nbt
                             new BlueprintMetadata("Tannery 1", "tannery_1"),
                             //tool_smith_1.nbt
@@ -87,7 +87,7 @@ public class ServerBlueprintManager {
                             //library_1.nbt
                             new BlueprintMetadata("Library 1", "library_1"),
                             //library_2.nbt
-                            new BlueprintMetadata("Library 2", "library_2"),
+                            new BlueprintMetadata("Library 2", "library_2", 1),
                             //temple_3.nbt
                             new BlueprintMetadata("Temple 3", "temple_3"),
                             //temple_4.nbt
@@ -98,19 +98,19 @@ public class ServerBlueprintManager {
                     BlueprintGroup.FARMS,
                     Arrays.asList(
                             //animal_pen_1.nbt
-                            new BlueprintMetadata("Animal Pen 1", "animal_pen_1"),
+                            new BlueprintMetadata("Animal Pen 1", "animal_pen_1", 1),
                             //animal_pen_2.nbt
-                            new BlueprintMetadata("Animal Pen 2", "animal_pen_2"),
+                            new BlueprintMetadata("Animal Pen 2", "animal_pen_2", 1),
                             //animal_pen_3.nbt
-                            new BlueprintMetadata("Animal Pen 3", "animal_pen_3"),
+                            new BlueprintMetadata("Animal Pen 3", "animal_pen_3", 1),
                             //small_farm_1.nbt
                             new BlueprintMetadata("Small Farm 1", "small_farm_1"),
                             //large_farm_1.nbt
                             new BlueprintMetadata("Large Farm 1", "large_farm_1"),
                             //stable_1.nbt
-                            new BlueprintMetadata("Stable 1", "stable_1"),
+                            new BlueprintMetadata("Stable 1", "stable_1", 1),
                             //stable_2.nbt
-                            new BlueprintMetadata("Stable 2", "stable_2")
+                            new BlueprintMetadata("Stable 2", "stable_2", 1)
                     )
             ),
             entry(
@@ -119,17 +119,17 @@ public class ServerBlueprintManager {
                             //accessory_1.nbt
                             new BlueprintMetadata("Accessory 1", "accessory_1"),
                             //fountain_01.nbt
-                            new BlueprintMetadata("Fountain 01", "fountain_01"),
+                            new BlueprintMetadata("Fountain 01", "fountain_01", 1),
                             //meeting_point_1.nbt
-                            new BlueprintMetadata("Meeting Point 1", "meeting_point_1"),
+                            new BlueprintMetadata("Meeting Point 1", "meeting_point_1", 1),
                             //meeting_point_2.nbt
-                            new BlueprintMetadata("Meeting Point 2", "meeting_point_2"),
+                            new BlueprintMetadata("Meeting Point 2", "meeting_point_2", 1),
                             //meeting_point_3.nbt
-                            new BlueprintMetadata("Meeting Point 3", "meeting_point_3"),
+                            new BlueprintMetadata("Meeting Point 3", "meeting_point_3", 1),
                             //meeting_point_4.nbt
-                            new BlueprintMetadata("Meeting Point 4", "meeting_point_4"),
+                            new BlueprintMetadata("Meeting Point 4", "meeting_point_4", 1),
                             //meeting_point_5.nbt
-                            new BlueprintMetadata("Meeting Point 5", "meeting_point_5")
+                            new BlueprintMetadata("Meeting Point 5", "meeting_point_5", 1)
                     )
             )
     );
