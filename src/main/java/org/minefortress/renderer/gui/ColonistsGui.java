7a92a231b714f95aa756c2a8a41dd49888ef5dad
Alexandr <lwjgl@mail.ru>
2022-03-15 20:31:08 +0100

    Add individual pawn GUI

--- src/main/java/org/minefortress/renderer/gui/ColonistsGui.java
+++ src/main/java/org/minefortress/renderer/gui/ColonistsGui.java
@@ -3,11 +3,18 @@ package org.minefortress.renderer.gui;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import org.minefortress.fortress.FortressClientManager;
 import org.minefortress.interfaces.FortressClientWorld;
@@ -30,10 +37,54 @@ public class ColonistsGui extends FortressGuiScreen{
     }
 
     @Override
-    void render(MatrixStack p, TextRenderer font, int screenWidth, int screenHeight, double mouseX, double mouseY, float delta) {
+    void render(MatrixStack matrices, TextRenderer font, int screenWidth, int screenHeight, double mouseX, double mouseY, float delta) {
         final FortressClientManager fortressManager = getFortressClientManager();
         if(!fortressManager.isInitialized()) return;
 
+        final boolean colonsitsCountHovered = renderColonistsCount(matrices, font, screenWidth, screenHeight, mouseX, mouseY);
+
+        if(fortressManager.isSelectingColonist()){
+            final int colonistWinX = 5;
+            final int colonistWinY = 25;
+            final int width = 120;
+            final int height = 125;
+            DrawableHelper.fillGradient(matrices, colonistWinX, colonistWinY, colonistWinX + width, colonistWinY + height, 0xc0101010, 0xd0101010, 100);
+
+            final String name = "<Pawn Name>";
+            final String healthString = "10/10";
+            final String hungerString = "10/10";
+            final String professionString = "Miner - 1 LVL";
+            final String tasksString = "Wondering around...";
+
+            Screen.drawCenteredText(matrices, font, name, colonistWinX + width / 2, colonistWinY + 5, 0xFFFFFF);
+
+            int heartIconX = colonistWinX + 5;
+            int heartIconY = colonistWinY + textRenderer.fontHeight + 10;
+            renderIcon(matrices, heartIconX, heartIconY, 8, 8, 52, 0);
+            textRenderer.draw(matrices, healthString, heartIconX + 10, heartIconY + 2, 0xFFFFFF);
+
+            int hungerIconX = colonistWinX + width/2 + 5;
+            int hungerIconY = heartIconY;
+            renderIcon(matrices, hungerIconX, hungerIconY, 8, 8, 52, 28);
+            textRenderer.draw(matrices, hungerString, hungerIconX + 10, hungerIconY + 2, 0xFFFFFF);
+
+            textRenderer.draw(matrices, "Profession:", colonistWinX + 5, hungerIconY + textRenderer.fontHeight + 5, 0xFFFFFF);
+            textRenderer.draw(matrices, professionString, colonistWinX + 5, hungerIconY + 2 * textRenderer.fontHeight + 5 , 0xFFFFFF);
+
+            textRenderer.draw(matrices, "Task:", colonistWinX + 5, hungerIconY + 3 * textRenderer.fontHeight + 10, 0xFFFFFF);
+            textRenderer.draw(matrices, tasksString, colonistWinX + 5, hungerIconY + 4 * textRenderer.fontHeight + 10, 0xFFFFFF);
+        }
+
+        this.hovered = colonsitsCountHovered;
+    }
+
+    private void renderIcon(MatrixStack matrices, int iconX, int iconY, int iconWidth, int iconHeight, int heartIconU, int heartIconV) {
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.setShaderTexture(0, GUI_ICONS_TEXTURE);
+        DrawableHelper.drawTexture(matrices, iconX, iconY, 110, heartIconU, heartIconV, iconWidth, iconHeight, 256, 256);
+    }
+
+    private boolean renderColonistsCount(MatrixStack p, TextRenderer font, int screenWidth, int screenHeight, double mouseX, double mouseY) {
         final String colonistsCountString = "x" + colonistsCount;
 
         final int iconX = screenWidth / 2 - 91;
@@ -41,28 +92,25 @@ public class ColonistsGui extends FortressGuiScreen{
         final float textX = screenWidth / 2f - 91 + 15;
         final int textY = screenHeight - 35;
 
-        final int boundLeftX = iconX;
         final int boundRightX = (int)textX + font.getWidth(colonistsCountString);
-        final int boundTopY = iconY;
         final int boundBottomY = iconY + 20;
 
-        this.hovered = mouseX >= boundLeftX && mouseX <= boundRightX && mouseY >= boundTopY && mouseY < boundBottomY;
-
-        RenderSystem.setShaderTexture(0, GUI_ICONS_TEXTURE);
+        final boolean hovered = mouseX >= iconX && mouseX <= boundRightX && mouseY >= iconY && mouseY < boundBottomY;
 
         super.itemRenderer.renderGuiItemIcon(new ItemStack(Items.PLAYER_HEAD), iconX, iconY);
 
         font.draw(p, colonistsCountString, textX, textY, 0xFFFFFF);
 
-        if(this.isHovered()) {
-            super.renderTooltip(p, Text.of("Your Pawns count"), (int)mouseX, (int)mouseY);
+        if(hovered) {
+            super.renderTooltip(p, Text.of("Your Pawns count"), (int) mouseX, (int) mouseY);
         }
+
+        return hovered;
     }
 
     private FortressClientManager getFortressClientManager() {
         final FortressMinecraftClient fortressClient = (FortressMinecraftClient) this.client;
-        final FortressClientManager fortressManager = fortressClient.getFortressClientManager();
-        return fortressManager;
+        return fortressClient.getFortressClientManager();
     }
 
     @Override
