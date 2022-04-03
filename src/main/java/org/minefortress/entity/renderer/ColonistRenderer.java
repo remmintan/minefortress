bfc1d67eab736be0ec9c3e4745c3c72517c524c5
Alexandr <lwjgl@mail.ru>
2022-04-03 10:13:46 +0200

    Add colonist profession skins and names to the client

--- src/main/java/org/minefortress/entity/renderer/ColonistRenderer.java
+++ src/main/java/org/minefortress/entity/renderer/ColonistRenderer.java
@@ -17,11 +17,35 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.*;
 import org.minefortress.entity.Colonist;
+import org.minefortress.fortress.FortressClientManager;
 import org.minefortress.interfaces.FortressMinecraftClient;
 
 public class ColonistRenderer extends BipedEntityRenderer<Colonist, BipedEntityModel<Colonist>> {
 
-    private static final Identifier TEXTURE = new Identifier("textures/entity/steve.png");
+    private static final Identifier STEVE = new Identifier("textures/entity/steve.png");
+    /*
+        baker.png
+        blacksmith.png
+        butcher.png
+        crafter.png
+        farmer.png
+        fisherman.png
+        hunter.png
+        lumberjack.png
+        medieval_underwear.png
+        miner.png
+     */
+    private static final Identifier BAKER = new Identifier("minefortress", "textures/skins/baker.png");
+    private static final Identifier BLACKSMITH = new Identifier("minefortress", "textures/skins/blacksmith.png");
+    private static final Identifier BUTCHER = new Identifier("minefortress", "textures/skins/butcher.png");
+    private static final Identifier CRAFTER = new Identifier("minefortress", "textures/skins/crafter.png");
+    private static final Identifier FARMER = new Identifier("minefortress", "textures/skins/farmer.png");
+    private static final Identifier FISHERMAN = new Identifier("minefortress", "textures/skins/fisherman.png");
+    private static final Identifier HUNTER = new Identifier("minefortress", "textures/skins/hunter.png");
+    private static final Identifier LUMBERJACK = new Identifier("minefortress", "textures/skins/lumberjack.png");
+    private static final Identifier MEDIEVAL_UNDERWEAR = new Identifier("minefortress", "textures/skins/medieval_underwear.png");
+    private static final Identifier MINER = new Identifier("minefortress", "textures/skins/miner.png");
+
 
     public ColonistRenderer(EntityRendererFactory.Context context) {
         super(context, new BipedEntityModel<>(context.getPart(EntityModelLayers.PLAYER)), 0.5f);
@@ -32,8 +56,8 @@ public class ColonistRenderer extends BipedEntityRenderer<Colonist, BipedEntityM
     }
 
     @Override
-    public Identifier getTexture(Colonist p_114482_) {
-        return TEXTURE;
+    public Identifier getTexture(Colonist colonist) {
+        return convertProfessionToSkin(colonist.getProfessionId());
     }
 
     @Override
@@ -44,15 +68,26 @@ public class ColonistRenderer extends BipedEntityRenderer<Colonist, BipedEntityM
     @Override
     public void render(Colonist mobEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
         super.render(mobEntity, f, g, matrixStack, vertexConsumerProvider, i);
-        final MinecraftClient client = MinecraftClient.getInstance();
-        final FortressMinecraftClient fortressClient = (FortressMinecraftClient) client;
+        final MinecraftClient client = getClient();
         final boolean hovering = client.crosshairTarget instanceof EntityHitResult entityHitResult && entityHitResult.getEntity() == mobEntity;
-        final boolean selecting = fortressClient.getFortressClientManager().getSelectedColonist() == mobEntity;
+        final boolean selecting = getFortressClientManager().getSelectedColonist() == mobEntity;
         if(hovering || selecting) {
             final VertexConsumer buffer = vertexConsumerProvider.getBuffer(RenderLayer.getLines());
             ColonistRenderer.renderRhombus(matrixStack, buffer, mobEntity, selecting);
         }
 
+    }
+
+    private MinecraftClient getClient() {
+        return MinecraftClient.getInstance();
+    }
+
+    private FortressMinecraftClient getFortressClient() {
+        return (FortressMinecraftClient) getClient();
+    }
+
+    private FortressClientManager getFortressClientManager() {
+        return getFortressClient().getFortressClientManager();
     }
 
     private static void renderRhombus(MatrixStack matrices, VertexConsumer vertices, Entity entity, boolean selecting) {
@@ -70,6 +105,36 @@ public class ColonistRenderer extends BipedEntityRenderer<Colonist, BipedEntityM
 
             WorldRenderer.drawBox(matrices, vertices, -0.5f,  -0.5f, -0.5f, 0.5f,  0.5f, 0.5f, selecting?0.0f:0.7f, selecting?1.0f:0.7f, selecting?0.0f:0.7f, 1.0f);
             matrices.pop();
+        }
+    }
+
+    private Identifier convertProfessionToSkin(String professionId) {
+        switch(professionId) {
+            case "baker":
+                return BAKER;
+            case "blacksmith":
+                return BLACKSMITH;
+            case "butcher":
+                return BUTCHER;
+            case "crafter":
+                return CRAFTER;
+            case "farmer":
+                return FARMER;
+            case "fisherman":
+                return FISHERMAN;
+            case "hunter":
+                return HUNTER;
+            case "lumberjack1":
+            case "lumberjack2":
+            case "lumberjack3":
+                return LUMBERJACK;
+            case "miner1":
+            case "miner2":
+            case "miner3":
+                return MINER;
+            case "colonist":
+            default:
+                return STEVE;
         }
     }
 }
