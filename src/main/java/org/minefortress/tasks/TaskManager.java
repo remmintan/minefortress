e2813d57679069de7fc9b9fec50e297c03cf84fb
Alexandr <lwjgl@mail.ru>
2022-04-03 18:24:41 +0200

    Add tasks manager

--- src/main/java/org/minefortress/tasks/TaskManager.java
+++ src/main/java/org/minefortress/tasks/TaskManager.java
@@ -1,13 +1,57 @@
 package org.minefortress.tasks;
 
+import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.NotNull;
+import org.minefortress.entity.Colonist;
+import org.minefortress.fortress.FortressServerManager;
 import org.minefortress.tasks.interfaces.Task;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class TaskManager {
 
+    private final static Set<String> BUILDER_PROFESSIONS = Set.of("miner1", "miner2", "miner3", "lumberjack1", "lumberjack2", "lumberjack3", "colonist");
+
     private final Deque<Task> tasks = new ArrayDeque<>();
     private final Set<UUID> cancelledTasks = new HashSet<>();
+
+    public void tick(FortressServerManager manager, ServerWorld world) {
+        while (this.hasTask()) {
+            final Task task = this.getTask();
+            final List<Colonist> freeColonists = manager.getFreeColonists();
+            if(freeColonists.isEmpty()) return;
+            final TaskType taskType = task.getTaskType();
+            if(taskType == TaskType.BUILD) {
+                final List<Colonist> completelyFreePawns = getCompletelyFreePawns(task, freeColonists);
+
+                boolean fullyCompleted = setPawnsToTask(world, task, completelyFreePawns);
+                if(fullyCompleted) continue;
+
+                final List<Colonist> otherPawns = freeColonists
+                        .stream()
+                        .filter(c -> isBuilderProfession(c.getProfessionId()))
+                        .filter(c -> c.getTaskControl().canStartTask(task))
+                        .filter(c -> c.getTaskControl().isDoingEverydayTasks())
+
+                        .collect(Collectors.toList());
+                setPawnsToTask(world, task, otherPawns);
+            } else {
+                final List<String> professions = getProfessionIdFromTask(task);
+                final List<Colonist> professionals = freeColonists
+                        .stream()
+                        .filter(c -> professions.contains(c.getProfessionId()))
+                        .filter(c -> c.getTaskControl().canStartTask(task))
+                        .collect(Collectors.toList());
+
+                boolean fullyCompleted = setPawnsToTask(world, task, professionals);
+                if(fullyCompleted) continue;
+
+                final List<Colonist> completelyFreePawns = getCompletelyFreePawns(task, freeColonists);
+                setPawnsToTask(world, task, completelyFreePawns);
+            }
+        }
+    }
 
     public void addTask(Task task) {
         task.prepareTask();
@@ -16,27 +60,52 @@ public class TaskManager {
         }
     }
 
-    public boolean hasTask() {
+    public void cancelTask(UUID id) {
+        cancelledTasks.add(id);
+        tasks.removeIf(task -> task.getId().equals(id));
+    }
+
+    @NotNull
+    private List<Colonist> getCompletelyFreePawns(Task task, List<Colonist> freeColonists) {
+        return freeColonists
+                .stream()
+                .filter(c -> c.getTaskControl().canStartTask(task))
+                .filter(c -> !c.getTaskControl().isDoingEverydayTasks())
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getProfessionIdFromTask(Task task) {
+        if(task instanceof CutTreesTask) {
+            return Arrays.asList("lumberjack1", "lumberjack2", "lumberjack3");
+        }
+        return Arrays.asList("miner1", "miner2", "miner3");
+    }
+
+    private static boolean isBuilderProfession(String professionId) {
+        return BUILDER_PROFESSIONS.contains(professionId);
+    }
+
+    private boolean setPawnsToTask(ServerWorld world, Task task, List<Colonist> completelyFreePawns) {
+        for(Colonist c : completelyFreePawns) {
+            if(!task.hasAvailableParts()) break;
+            c.getTaskControl().setTask(task, task.getNextPart(world), this::returnTaskPart, () -> this.isCancelled(task.getId()));
+        }
+        if(!task.hasAvailableParts()) {
+            tasks.remove();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasTask() {
         return !tasks.isEmpty();
     }
 
-    public Task getTask() {
+    private Task getTask() {
         return tasks.element();
     }
 
-    public boolean nextTaskIdIsNotIn(Set<UUID> set) {
-        if (tasks.peek() != null) {
-            return !set.contains(tasks.peek().getId());
-        }
-
-        return true;
-    }
-
-    public void removeTask() {
-        tasks.remove();
-    }
-
-    public void returnTaskPart(TaskPart taskPart) {
+    private void returnTaskPart(TaskPart taskPart) {
         Task task = taskPart.getTask();
         task.returnPart(taskPart.getStartAndEnd());
 
@@ -45,12 +114,7 @@ public class TaskManager {
         }
     }
 
-    public void cancelTask(UUID id) {
-        cancelledTasks.add(id);
-        tasks.removeIf(task -> task.getId().equals(id));
-    }
-
-    public boolean isCancelled(UUID id) {
+    private boolean isCancelled(UUID id) {
         return cancelledTasks.contains(id);
     }
 
