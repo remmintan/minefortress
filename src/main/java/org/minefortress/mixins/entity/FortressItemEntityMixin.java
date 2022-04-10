16980e21fd37a1f8dbf5fc4a55b0da31237ff7be
Alexandr <lwjgl@mail.ru>
2022-04-10 20:53:58 +0200

    Allow to pickup items, prevent items from staying on the ground in blueprints world
    (cherry picked from commit d52215089487e97855a2560e275f485a54f6e18d)

--- src/main/java/org/minefortress/mixins/entity/FortressItemEntityMixin.java
+++ src/main/java/org/minefortress/mixins/entity/FortressItemEntityMixin.java
@@ -9,13 +9,18 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
+import org.minefortress.blueprints.world.BlueprintsWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemEntity.class)
 public abstract class FortressItemEntityMixin extends Entity {
+
+    @Shadow private int itemAge;
 
     public FortressItemEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -25,8 +30,19 @@ public abstract class FortressItemEntityMixin extends Entity {
     void disablePickup(PlayerEntity player, CallbackInfo ci) {
         final ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
         final ServerPlayerInteractionManager interactionManager = serverPlayer.interactionManager;
-        if(interactionManager.getGameMode() == ClassTinkerers.getEnum(GameMode.class, "FORTRESS")) {
+        final boolean notBlueprintsWorld = player.world.getRegistryKey() != BlueprintsWorld.BLUEPRINTS_WORLD_REGISTRY_KEY;
+        final boolean isFortressGamemode = interactionManager.getGameMode() == ClassTinkerers.getEnum(GameMode.class, "FORTRESS");
+        if(isFortressGamemode && notBlueprintsWorld) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "getItemAge", at = @At("HEAD"), cancellable = true)
+    void getItemAge(CallbackInfoReturnable<Integer> cir) {
+        if(this.world.getRegistryKey() == BlueprintsWorld.BLUEPRINTS_WORLD_REGISTRY_KEY){
+            cir.setReturnValue(6000);
+        } else {
+            cir.setReturnValue(this.itemAge);
         }
     }
 
