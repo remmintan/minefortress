3b25eaa26e3f848270c001211e5f8c9a11523f1b
Alexandr <lwjgl@mail.ru>
2022-04-11 22:59:10 +0200

    Add campfire renderer
    (cherry picked from commit 19c8958821e5b5eb221f2d7f5a04265e7ec91c80)

---
+++ src/main/java/org/minefortress/renderer/custom/BuiltModel.java
@@ -0,0 +1,11 @@
+package org.minefortress.renderer.custom;
+
+import net.minecraft.client.gl.VertexBuffer;
+import net.minecraft.client.render.RenderLayer;
+
+public interface BuiltModel {
+
+    boolean hasLayer(RenderLayer layer);
+    VertexBuffer getBuffer(RenderLayer layer);
+
+}
