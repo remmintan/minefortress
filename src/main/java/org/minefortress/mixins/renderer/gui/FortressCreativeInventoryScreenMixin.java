34967a7510caf7c810e37b1bedb77bc0fd5426ce
Alexandr <lwjgl@mail.ru>
2022-04-21 00:36:39 +0200

    Remove all tabs from the inventory

--- src/main/java/org/minefortress/mixins/renderer/gui/FortressCreativeInventoryScreenMixin.java
+++ src/main/java/org/minefortress/mixins/renderer/gui/FortressCreativeInventoryScreenMixin.java
@@ -17,9 +17,11 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.GameMode;
+import org.jetbrains.annotations.Nullable;
 import org.minefortress.fortress.FortressClientManager;
 import org.minefortress.interfaces.FortressMinecraftClient;
 import org.minefortress.renderer.gui.resources.FortressSurvivalInventoryScreenHandler;
@@ -30,6 +32,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class FortressCreativeInventoryScreenMixin extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler> {
@@ -45,6 +48,9 @@ public abstract class FortressCreativeInventoryScreenMixin extends AbstractInven
 
     @Shadow protected abstract boolean hasScrollbar();
 
+    @Shadow private @Nullable Slot deleteItemSlot;
+    @Shadow @Final private static Text DELETE_ITEM_SLOT_TEXT;
+    @Shadow private boolean scrolling;
     private static final GameMode FORTRESS_GAMEMODE = ClassTinkerers.getEnum(GameMode.class, "FORTRESS");
 
     public FortressCreativeInventoryScreenMixin(CreativeInventoryScreen.CreativeScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
@@ -53,10 +59,14 @@ public abstract class FortressCreativeInventoryScreenMixin extends AbstractInven
 
     @Inject(method = "<init>", at = @At("RETURN"))
     void init(PlayerEntity player, CallbackInfo ci) {
-        if(isFortressGamemode() && !getClientManager().isCreative()){
+        if(isFortressGamemode() && isNotCreative()){
             super.handler = new FortressSurvivalInventoryScreenHandler(player);
             player.currentScreenHandler = super.handler;
         }
+    }
+
+    private boolean isNotCreative() {
+        return !getClientManager().isCreative();
     }
 
     @Redirect(method = "onMouseClick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;dropItem(Lnet/minecraft/item/ItemStack;Z)Lnet/minecraft/entity/ItemEntity;"))
@@ -75,32 +85,72 @@ public abstract class FortressCreativeInventoryScreenMixin extends AbstractInven
         }
     }
 
-    @Override
-    public void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-        ItemGroup itemGroup = ItemGroup.GROUPS[selectedTab];
+    @Inject(method = "mouseReleased", at = @At("HEAD"), cancellable = true)
+    public void mouseReleased(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        if(isFortressGamemode() && isNotCreative()) {
+            if (button == 0) {
+                double d = mouseX - (double)this.x;
+                double e = mouseY - (double)this.y;
+                this.scrolling = false;
+//            for (ItemGroup itemGroup : ItemGroup.GROUPS) {
+//                if (!this.isClickInTab(itemGroup, d, e)) continue;
+//                this.setSelectedTab(itemGroup);
+//                return true;
+//            }
+            }
+            cir.setReturnValue(super.mouseReleased(mouseX, mouseY, button));
+        }
+    }
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        if(isFortressGamemode() && isNotCreative()) {
+            this.renderBackground(matrices);
+            super.render(matrices, mouseX, mouseY, delta);
+//        for (ItemGroup itemGroup : ItemGroup.GROUPS) {
+//            if (this.renderTabTooltipIfHovered(matrices, itemGroup, mouseX, mouseY)) break;
+//        }
+            if (this.deleteItemSlot != null && selectedTab == ItemGroup.INVENTORY.getIndex() && this.isPointWithinBounds(this.deleteItemSlot.x, this.deleteItemSlot.y, 16, 16, mouseX, mouseY)) {
+                this.renderTooltip(matrices, DELETE_ITEM_SLOT_TEXT, mouseX, mouseY);
+            }
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+            this.drawMouseoverTooltip(matrices, mouseX, mouseY);
+
+            ci.cancel();
+        }
+    }
+
+
+    @Inject(method = "drawBackground", at = @At("HEAD"), cancellable = true)
+    public void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY, CallbackInfo ci) {
+        if(isFortressGamemode() && isNotCreative()) {
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+            ItemGroup itemGroup = ItemGroup.GROUPS[selectedTab];
 //        for (ItemGroup itemGroup2 : ItemGroup.GROUPS) {
 //            RenderSystem.setShader(GameRenderer::getPositionTexShader);
 //            RenderSystem.setShaderTexture(0, TEXTURE);
 //            if (itemGroup2.getIndex() == selectedTab) continue;
 //            this.renderTabIcon(matrices, itemGroup2);
 //        }
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, new Identifier(TAB_TEXTURE_PREFIX + itemGroup.getTexture()));
-        this.drawTexture(matrices, this.x, this.y, 0, 0, this.backgroundWidth, this.backgroundHeight);
-        this.searchBox.render(matrices, mouseX, mouseY, delta);
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-        int i = this.x + 175;
-        int j = this.y + 18;
-        int k = j + 112;
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        if (itemGroup.hasScrollbar()) {
-            this.drawTexture(matrices, i, j + (int)((float)(k - j - 17) * this.scrollPosition), 232 + (this.hasScrollbar() ? 0 : 12), 0, 12, 15);
-        }
-        this.renderTabIcon(matrices, itemGroup);
-        if (itemGroup == ItemGroup.INVENTORY) {
-            InventoryScreen.drawEntity(this.x + 88, this.y + 45, 20, this.x + 88 - mouseX, this.y + 45 - 30 - mouseY, this.client.player);
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderTexture(0, new Identifier(TAB_TEXTURE_PREFIX + itemGroup.getTexture()));
+            this.drawTexture(matrices, this.x, this.y, 0, 0, this.backgroundWidth, this.backgroundHeight);
+            this.searchBox.render(matrices, mouseX, mouseY, delta);
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+            int i = this.x + 175;
+            int j = this.y + 18;
+            int k = j + 112;
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderTexture(0, TEXTURE);
+            if (itemGroup.hasScrollbar()) {
+                this.drawTexture(matrices, i, j + (int)((float)(k - j - 17) * this.scrollPosition), 232 + (this.hasScrollbar() ? 0 : 12), 0, 12, 15);
+            }
+            this.renderTabIcon(matrices, itemGroup);
+            if (itemGroup == ItemGroup.INVENTORY) {
+                InventoryScreen.drawEntity(this.x + 88, this.y + 45, 20, this.x + 88 - mouseX, this.y + 45 - 30 - mouseY, this.client.player);
+            }
+
+            ci.cancel();
         }
     }
 
