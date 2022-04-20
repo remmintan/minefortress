68ee2ead6a7a9dec487fc13342858c2654595459
Alexandr <lwjgl@mail.ru>
2022-04-20 23:28:49 +0200

    Add drawing tabs to Fortress resource manager

--- src/main/java/org/minefortress/mixins/renderer/gui/FortressCreativeInventoryScreenMixin.java
+++ src/main/java/org/minefortress/mixins/renderer/gui/FortressCreativeInventoryScreenMixin.java
@@ -1,22 +1,31 @@
 package org.minefortress.mixins.renderer.gui;
 
 import com.chocohead.mm.api.ClassTinkerers;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.GameMode;
 import org.minefortress.fortress.FortressClientManager;
 import org.minefortress.interfaces.FortressMinecraftClient;
 import org.minefortress.renderer.gui.resources.FortressSurvivalInventoryScreenHandler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -24,6 +33,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class FortressCreativeInventoryScreenMixin extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler> {
+
+    @Shadow @Final private static Identifier TEXTURE;
+    @Shadow @Final private static String TAB_TEXTURE_PREFIX;
+    @Shadow private static int selectedTab;
+
+    @Shadow protected abstract void renderTabIcon(MatrixStack matrices, ItemGroup group);
+
+    @Shadow private TextFieldWidget searchBox;
+    @Shadow private float scrollPosition;
+
+    @Shadow protected abstract boolean hasScrollbar();
 
     private static final GameMode FORTRESS_GAMEMODE = ClassTinkerers.getEnum(GameMode.class, "FORTRESS");
 
@@ -55,6 +75,34 @@ public abstract class FortressCreativeInventoryScreenMixin extends AbstractInven
         }
     }
 
+    @Override
+    public void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        ItemGroup itemGroup = ItemGroup.GROUPS[selectedTab];
+//        for (ItemGroup itemGroup2 : ItemGroup.GROUPS) {
+//            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+//            RenderSystem.setShaderTexture(0, TEXTURE);
+//            if (itemGroup2.getIndex() == selectedTab) continue;
+//            this.renderTabIcon(matrices, itemGroup2);
+//        }
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, new Identifier(TAB_TEXTURE_PREFIX + itemGroup.getTexture()));
+        this.drawTexture(matrices, this.x, this.y, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        this.searchBox.render(matrices, mouseX, mouseY, delta);
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        int i = this.x + 175;
+        int j = this.y + 18;
+        int k = j + 112;
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        if (itemGroup.hasScrollbar()) {
+            this.drawTexture(matrices, i, j + (int)((float)(k - j - 17) * this.scrollPosition), 232 + (this.hasScrollbar() ? 0 : 12), 0, 12, 15);
+        }
+        this.renderTabIcon(matrices, itemGroup);
+        if (itemGroup == ItemGroup.INVENTORY) {
+            InventoryScreen.drawEntity(this.x + 88, this.y + 45, 20, this.x + 88 - mouseX, this.y + 45 - 30 - mouseY, this.client.player);
+        }
+    }
 
     private boolean isFortressGamemode() {
         return getClient().interactionManager != null && FORTRESS_GAMEMODE == getClient().interactionManager.getCurrentGameMode();
