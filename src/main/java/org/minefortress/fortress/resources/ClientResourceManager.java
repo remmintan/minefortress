97e1324cf3ef31b963f91c131cf6c886577f3cf2
Alexandr <lwjgl@mail.ru>
2022-04-22 23:40:18 +0200

    Add client resource manager

---
+++ src/main/java/org/minefortress/fortress/resources/ClientResourceManager.java
@@ -0,0 +1,17 @@
+package org.minefortress.fortress.resources;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ClientResourceManager {
+
+    Set<ItemGroup> getGroups();
+    boolean hasStacks(List<ItemStack> stacks);
+    List<ItemStack> getStacks(ItemGroup group);
+    void setItemAmount(Item item, int amount);
+
+}
