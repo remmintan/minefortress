9032ed0e167985901004f65148d594d4e552fc4c
Alexandr <lwjgl@mail.ru>
2022-04-24 14:51:31 +0200

    Update client resource manager to keep information live and up to date
    (cherry picked from commit de1cf3cc366af681b65a82546433438c5fc028a9)

---
+++ src/main/java/org/minefortress/fortress/resources/client/ClientItemStacksManager.java
@@ -0,0 +1,27 @@
+package org.minefortress.fortress.resources.client;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class ClientItemStacksManager {
+
+    private Map<Item, FortressItemStack> stacks = new HashMap<>();
+
+    public FortressItemStack getStack(Item item) {
+        return stacks.computeIfAbsent(item, i -> new FortressItemStack(i, 0));
+    }
+
+    boolean isEmpty() {
+        return stacks.values().stream().allMatch(FortressItemStack::isEmpty);
+    }
+
+    List<ItemStack> getStacks() {
+        return new ArrayList<>(stacks.values());
+    }
+
+}
