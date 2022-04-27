9e9847c0a97aeaaebda5652746746f1b38f83270
Alexandr <lwjgl@mail.ru>
2022-04-28 00:56:55 +0200

    Fix successful draw of crafting table

--- src/main/java/org/minefortress/fortress/resources/craft/FortressCraftingScreen.java
+++ src/main/java/org/minefortress/fortress/resources/craft/FortressCraftingScreen.java
@@ -5,8 +5,12 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.minefortress.fortress.resources.client.FortressItemStack;
+
+import java.util.Arrays;
 
 public class FortressCraftingScreen extends HandledScreen<FortressCraftingScreenHandler> {
 
@@ -14,6 +18,22 @@ public class FortressCraftingScreen extends HandledScreen<FortressCraftingScreen
 
     public FortressCraftingScreen(FortressCraftingScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        this.titleX = 29;
+        this.handler.setItems(Arrays.asList(
+                new FortressItemStack(Items.DIRT, 32)
+        ));
+        this.handler.scrollItems(0f);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        super.render(matrices, mouseX, mouseY, delta);
     }
 
     @Override
--- src/main/java/org/minefortress/fortress/resources/craft/FortressCraftingScreenHandler.java
+++ src/main/java/org/minefortress/fortress/resources/craft/FortressCraftingScreenHandler.java
@@ -4,12 +4,19 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.CraftingResultInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeMatcher;
 import net.minecraft.recipe.book.RecipeBookCategory;
 import net.minecraft.screen.AbstractRecipeScreenHandler;
+import net.minecraft.screen.slot.CraftingResultSlot;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 import static org.minefortress.MineFortressMod.FORTRESS_CRAFTING_SCREEN_HANDLER;
 
@@ -18,11 +25,30 @@ public class FortressCraftingScreenHandler extends AbstractRecipeScreenHandler<C
     private final CraftingInventory input = new CraftingInventory(this, 3, 3);
     private final CraftingResultInventory result = new CraftingResultInventory();
 
+    private final DefaultedList<ItemStack> items = DefaultedList.of();
+    private final Inventory tableInventory = new SimpleInventory(46);
+
     private final World world;
 
     public FortressCraftingScreenHandler(int syncId, PlayerInventory inventory) {
         super(FORTRESS_CRAFTING_SCREEN_HANDLER, syncId);
-        this.world = inventory.player.world;
+        final var player = inventory.player;
+        this.world = player.world;
+        
+        this.addSlot(new CraftingResultSlot(player, this.input, this.result, 0, 124, 35));
+        for (int row = 0; row < 3; ++row) {
+            for (int column = 0; column < 3; ++column) {
+                this.addSlot(new Slot(this.input, column + row * 3, 30 + column * 18, 17 + row * 18));
+            }
+        }
+        for (int row = 0; row < 3; ++row) {
+            for (int column = 0; column < 9; ++column) {
+                this.addSlot(new Slot(tableInventory, column + row * 9 + 9, 8 + column * 18, 84 + row * 18));
+            }
+        }
+        for (int column = 0; column < 9; ++column) {
+            this.addSlot(new Slot(tableInventory, column, 8 + column * 18, 142));
+        }
     }
 
     @Override
@@ -80,6 +106,35 @@ public class FortressCraftingScreenHandler extends AbstractRecipeScreenHandler<C
     public ItemStack transferSlot(PlayerEntity player, int index) {
         if(index != 0) return ItemStack.EMPTY;
         return super.transferSlot(player, index);
+    }
+
+    @Override
+    public void updateSlotStacks(int revision, List<ItemStack> stacks, ItemStack cursorStack) {
+        this.cursorStack = cursorStack;
+        this.revision = revision;
+    }
+
+    public void scrollItems(float position) {
+        int i = (this.items.size() + 9 - 1) / 9 - 5;
+        int j = (int)((double)(position * (float)i) + 0.5);
+        if (j < 0) {
+            j = 0;
+        }
+        for (int k = 0; k < 5; ++k) {
+            for (int l = 0; l < 9; ++l) {
+                int m = l + (k + j) * 9;
+                if (m >= 0 && m < this.items.size()) {
+                    tableInventory.setStack(l + k * 9, this.items.get(m));
+                    continue;
+                }
+                tableInventory.setStack(l + k * 9, ItemStack.EMPTY);
+            }
+        }
+    }
+
+    public void setItems(List<ItemStack> items) {
+        this.items.clear();
+        this.items.addAll(items);
     }
 
 }
