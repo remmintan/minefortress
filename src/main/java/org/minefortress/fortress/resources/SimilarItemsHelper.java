8733a2f69e9421ddcc94bc7fec2efa812ff60ed2
Alexandr <lwjgl@mail.ru>
2022-05-15 23:29:30 +0200

    Add stripped logs and wood to the similar items
    (cherry picked from commit fd9a18fcfca8dd165e5946e07e3b581ac632d9f0)

--- src/main/java/org/minefortress/fortress/resources/SimilarItemsHelper.java
+++ src/main/java/org/minefortress/fortress/resources/SimilarItemsHelper.java
@@ -1,6 +1,7 @@
 package org.minefortress.fortress.resources;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.tag.ItemTags;
 import net.minecraft.tag.Tag;
 
@@ -10,6 +11,28 @@ import java.util.List;
 import java.util.Optional;
 
 public class SimilarItemsHelper {
+
+    private static final List<Item> strippedLogs = Arrays.asList(
+            Items.STRIPPED_ACACIA_LOG,
+            Items.STRIPPED_BIRCH_LOG,
+            Items.STRIPPED_DARK_OAK_LOG,
+            Items.STRIPPED_JUNGLE_LOG,
+            Items.STRIPPED_OAK_LOG,
+            Items.STRIPPED_SPRUCE_LOG,
+            Items.STRIPPED_WARPED_STEM,
+            Items.STRIPPED_CRIMSON_STEM
+    );
+
+    private static final List<Item> strippedWood = Arrays.asList(
+            Items.STRIPPED_ACACIA_WOOD,
+            Items.STRIPPED_BIRCH_WOOD,
+            Items.STRIPPED_DARK_OAK_WOOD,
+            Items.STRIPPED_JUNGLE_WOOD,
+            Items.STRIPPED_OAK_WOOD,
+            Items.STRIPPED_SPRUCE_WOOD,
+            Items.STRIPPED_WARPED_HYPHAE,
+            Items.STRIPPED_CRIMSON_HYPHAE
+    );
 
     private static final List<Tag.Identified<Item>> tags = Arrays.asList(
             ItemTags.WOODEN_BUTTONS,
@@ -23,10 +46,19 @@ public class SimilarItemsHelper {
             ItemTags.SIGNS,
             ItemTags.FENCES,
             ItemTags.LEAVES,
-            ItemTags.FLOWERS
+            ItemTags.FLOWERS,
+            ItemTags.LOGS
     );
 
     public static List<Item> getSimilarItems(Item item) {
+        if(strippedLogs.contains(item)) {
+            return strippedLogs;
+        }
+
+        if(strippedWood.contains(item)) {
+            return strippedWood;
+        }
+
         return getItemTag(item)
                 .map(tag -> tag.values().stream().filter(it -> it != item).toList())
                 .orElse(Collections.emptyList());
