25632b25655437155731e3b6b98e9906b2a771ca
Alexandr <lwjgl@mail.ru>
2022-05-17 23:23:26 +0200

    Split craftin screen handler into abstract and concreet part

--- src/main/java/org/minefortress/fortress/resources/craft/FortressRecipeBookWidget.java
+++ src/main/java/org/minefortress/fortress/resources/craft/FortressRecipeBookWidget.java
@@ -3,6 +3,8 @@ package org.minefortress.fortress.resources.craft;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import net.minecraft.client.recipebook.ClientRecipeBook;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.screen.AbstractRecipeScreenHandler;
 import org.minefortress.interfaces.FortressSimpleInventory;
@@ -12,26 +14,31 @@ import java.util.ArrayList;
 public class FortressRecipeBookWidget extends RecipeBookWidget {
 
     private int cachedInvChangeCount;
+    private final RecipeType recipeType;
+
+    public FortressRecipeBookWidget(RecipeType<? extends Recipe<? extends Inventory>> recipeType) {
+        this.recipeType = recipeType;
+    }
 
     @Override
-    public void initialize(int parentWidth, int parentHeight, MinecraftClient client, boolean narrow, AbstractRecipeScreenHandler<?> craftingScreenHandler) {
+    public void initialize(int parentWidth, int parentHeight, MinecraftClient client, boolean narrow, AbstractRecipeScreenHandler<?> handler) {
         this.client = client;
-        this.craftingScreenHandler = craftingScreenHandler;
-        client.player.currentScreenHandler = craftingScreenHandler;
+        this.craftingScreenHandler = handler;
+        client.player.currentScreenHandler = handler;
         this.recipeBook = client.player.getRecipeBook();
         client.keyboard.setRepeatEvents(true);
 
-        final var craftingRecipes = client.world.getRecipeManager().listAllOfType(RecipeType.CRAFTING);
+        final var craftingRecipes = client.world.getRecipeManager().listAllOfType(recipeType);
         craftingRecipes
                 .forEach(res -> {
-                    this.recipeBook.add(res);
-                    this.recipeBook.display(res);
+                    this.recipeBook.add((Recipe<?>) res);
+                    this.recipeBook.display((Recipe<?>) res);
                 });
 
         this.recipeBook = new ClientRecipeBook();
         this.recipeBook.reload(new ArrayList<>(craftingRecipes));
         this.recipeBook.setGuiOpen(this.craftingScreenHandler.getCategory(), true);
-        super.initialize(parentWidth, parentHeight, client, narrow, craftingScreenHandler);
+        super.initialize(parentWidth, parentHeight, client, narrow, handler);
         this.setOpen(true);
 
     }
