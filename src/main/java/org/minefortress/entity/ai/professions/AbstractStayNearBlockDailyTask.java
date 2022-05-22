f16f23fd8953d04442129a779cfa5a6586f3e4b9
Alexandr <lwjgl@mail.ru>
2022-05-22 15:17:59 +0200

    Add exhaustion when wandering around the village

--- src/main/java/org/minefortress/entity/ai/professions/AbstractStayNearBlockDailyTask.java
+++ src/main/java/org/minefortress/entity/ai/professions/AbstractStayNearBlockDailyTask.java
@@ -32,7 +32,7 @@ abstract class AbstractStayNearBlockDailyTask implements ProfessionDailyTask {
             if(workingTicks % 10 * colonist.getHungerMultiplier() == 0) {
                 colonist.swingHand(colonist.world.random.nextFloat() < 0.5F? Hand.MAIN_HAND : Hand.OFF_HAND);
                 colonist.putItemInHand(getWorkingItem());
-                colonist.addExhaustion(0.1f);
+                colonist.addExhaustion(0.05f);
             }
             colonist.lookAt(blockPos);
             workingTicks++;
