abcfcb96fc6810f6e62824e9f6dde644d77464d2
Alexandr <lwjgl@mail.ru>
2022-05-22 17:16:30 +0200

    Add Forester daily task
    (cherry picked from commit 10d1136f863b416afaee5b7f79db69dc75a6982e)

--- src/main/java/org/minefortress/entity/ai/goal/DailyProfessionTasksGoal.java
+++ src/main/java/org/minefortress/entity/ai/goal/DailyProfessionTasksGoal.java
@@ -5,6 +5,7 @@ import org.minefortress.entity.Colonist;
 import org.minefortress.entity.ai.controls.TaskControl;
 import org.minefortress.entity.ai.professions.BlacksmithDailyTask;
 import org.minefortress.entity.ai.professions.CrafterDailyTask;
+import org.minefortress.entity.ai.professions.ForesterDailyTask;
 import org.minefortress.entity.ai.professions.ProfessionDailyTask;
 
 import java.util.EnumSet;
@@ -17,7 +18,8 @@ public class DailyProfessionTasksGoal extends Goal {
     private final Colonist colonist;
     private final Map<String, ProfessionDailyTask> dailyTasks = Map.ofEntries(
             entry("crafter", new CrafterDailyTask()),
-            entry("blacksmith", new BlacksmithDailyTask())
+            entry("blacksmith", new BlacksmithDailyTask()),
+            entry("forester", new ForesterDailyTask())
     );
 
     private ProfessionDailyTask currentTask;
