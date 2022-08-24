2392c614f764abb5c9293dcab19deb471814b657
Alexandr <lwjgl@mail.ru>
2022-08-25 00:00:51 +0200

    Adapt add blueprint screen

--- src/main/java/org/minefortress/renderer/gui/blueprints/AddBlueprintScreen.java
+++ src/main/java/org/minefortress/renderer/gui/blueprints/AddBlueprintScreen.java
@@ -1,8 +1,10 @@
 package org.minefortress.renderer.gui.blueprints;
 
+import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import org.apache.logging.log4j.util.Strings;
 import org.minefortress.network.ServerboundEditBlueprintPacket;
@@ -44,5 +46,22 @@ public class AddBlueprintScreen extends Screen {
 
         );
         this.addDrawableChild(btn);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        GameMenuScreen.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 40, 0xFFFFFF);
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public void close() {
+        if(this.client != null) this.client.setScreen(new BlueprintsScreen());
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
     }
 }
