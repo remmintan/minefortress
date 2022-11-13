084026d24fa21e0488f7da342dc820e275a18e91
Alexandr <lwjgl@mail.ru>
2022-11-13 22:47:53 +0100

    Remove unused network channels

--- src/main/java/org/minefortress/network/helpers/FortressChannelNames.java
+++ src/main/java/org/minefortress/network/helpers/FortressChannelNames.java
@@ -28,8 +28,6 @@ public class FortressChannelNames {
     public static final String FORTRESS_RESOURCES_SYNC = "fortress_resources_sync";
     public static final String FORTRESS_OPEN_CRAFTING_TABLE = "fortress_open_crafting_table";
     public static final String SCROLL_CURRENT_SCREEN = "scroll_current_screen";
-    public static final String FORTRESS_COMBAT_STATE_SYNC = "fortress_combat_state_sync";
-    public static final String FORTRESS_SET_COMBAT_STATE = "fortress_set_combat_state";
     public static final String FORTRESS_SLEEP = "fortress_sleep";
     public static final String FORTRESS_CHANGE_MAX_COLONISTS_COUNT = "fortress_change_max_colonists_count";
     public static final String FORTRESS_BLUEPRINTS_IMPORT_EXPORT = "fortress_blueprints_import_export";
