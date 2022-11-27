6d14805a569595adcaf201a82121bd66baa2d63c
remmintan <remmintan@gmail.com>
2022-11-27 16:15:33 +0100

    Fix mod screen pausing the game

--- src/main/java/org/minefortress/renderer/gui/ChooseModeScreen.java
+++ src/main/java/org/minefortress/renderer/gui/ChooseModeScreen.java
@@ -4,9 +4,10 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
-import org.minefortress.fortress.FortressClientManager;
 import org.minefortress.fortress.FortressGamemode;
-import org.minefortress.interfaces.FortressMinecraftClient;
+import org.minefortress.utils.ModUtils;
+
+import java.util.Objects;
 
 public class ChooseModeScreen extends Screen {
 
@@ -19,18 +20,18 @@ public class ChooseModeScreen extends Screen {
     @Override
     protected void init() {
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 24 - 16, 204, 20, new LiteralText("Creative"), button -> {
-            getClientManager().setGamemode(FortressGamemode.CREATIVE);
+            ModUtils.getFortressClientManager().setGamemode(FortressGamemode.CREATIVE);
             closeMenu();
         }));
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 48 - 16, 204, 20, new LiteralText("Survival"), button -> {
-            getClientManager().setGamemode(FortressGamemode.SURVIVAL);
+            ModUtils.getFortressClientManager().setGamemode(FortressGamemode.SURVIVAL);
             closeMenu();
         }));
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        if(!getClientManager().gamemodeNeedsInitialization()) this.client.setScreen(null);
+        if(!ModUtils.getFortressClientManager().gamemodeNeedsInitialization()) Objects.requireNonNull(this.client).setScreen(null);
         super.renderBackground(matrices);
         ChooseModeScreen.drawCenteredText(matrices, this.textRenderer, questionText, this.width / 2, 40, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
@@ -41,11 +42,12 @@ public class ChooseModeScreen extends Screen {
         return false;
     }
 
-    private void closeMenu() {
-        this.client.setScreen(null);
+    @Override
+    public boolean shouldPause() {
+        return false;
     }
 
-    private FortressClientManager getClientManager() {
-        return ((FortressMinecraftClient)this.client).getFortressClientManager();
+    private void closeMenu() {
+        Objects.requireNonNull(this.client).setScreen(null);
     }
 }
