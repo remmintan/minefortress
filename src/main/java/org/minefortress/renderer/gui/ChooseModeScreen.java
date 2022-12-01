54cf5275ead55c09f2799c334beb4e71796c1426
remmintan <remmintan@gmail.com>
2022-12-01 19:22:23 +0100

    Add ChooseModeScreen loading

--- src/main/java/org/minefortress/renderer/gui/ChooseModeScreen.java
+++ src/main/java/org/minefortress/renderer/gui/ChooseModeScreen.java
@@ -12,6 +12,8 @@ import java.util.Objects;
 public class ChooseModeScreen extends Screen {
 
     private final LiteralText questionText = new LiteralText("Choose a gamemode for this world");
+    private final LiteralText loadingText = new LiteralText("Loading...");
+    private boolean loading = false;
 
     public  ChooseModeScreen() {
         super(new LiteralText("Choose Game Mode"));
@@ -20,12 +22,12 @@ public class ChooseModeScreen extends Screen {
     @Override
     protected void init() {
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 24 - 16, 204, 20, new LiteralText("Creative"), button -> {
+            setLoading();
             ModUtils.getFortressClientManager().setGamemode(FortressGamemode.CREATIVE);
-            closeMenu();
         }));
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 48 - 16, 204, 20, new LiteralText("Survival"), button -> {
+            setLoading();
             ModUtils.getFortressClientManager().setGamemode(FortressGamemode.SURVIVAL);
-            closeMenu();
         }));
     }
 
@@ -33,7 +35,8 @@ public class ChooseModeScreen extends Screen {
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         if(!ModUtils.getFortressClientManager().gamemodeNeedsInitialization()) Objects.requireNonNull(this.client).setScreen(null);
         super.renderBackground(matrices);
-        ChooseModeScreen.drawCenteredText(matrices, this.textRenderer, questionText, this.width / 2, 40, 0xFFFFFF);
+        ChooseModeScreen.drawCenteredText(matrices, this.textRenderer, loading?loadingText:questionText, this.width / 2, 40, 0xFFFFFF);
+        if(loading) return;
         super.render(matrices, mouseX, mouseY, delta);
     }
 
@@ -47,7 +50,7 @@ public class ChooseModeScreen extends Screen {
         return false;
     }
 
-    private void closeMenu() {
-        Objects.requireNonNull(this.client).setScreen(null);
+    private void setLoading() {
+        this.loading = true;
     }
 }
