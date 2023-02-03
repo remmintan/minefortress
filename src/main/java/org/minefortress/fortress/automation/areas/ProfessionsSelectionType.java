d7e7976cabe79c5cdc4c263c0d0ac22f3455617f
remmintan <remmintan@gmail.com>
2023-02-03 23:17:30 +0100

    Commented some not released profession types

--- src/main/java/org/minefortress/fortress/automation/areas/ProfessionsSelectionType.java
+++ src/main/java/org/minefortress/fortress/automation/areas/ProfessionsSelectionType.java
@@ -13,24 +13,24 @@ public enum ProfessionsSelectionType {
             new Vector4f(0.5f, 0.5f, 0.5f, 1.0f),
             (reqString) -> reqString.startsWith("miner")
     ),
-    LOGGING(
-            "Logging",
-            Items.STONE_AXE,
-            new Vector4f(0.3f, 0.6f, 0.0f, 1.0f),
-            (reqString) -> reqString.startsWith("lumberjack")
-    ),
+//    LOGGING(
+//            "Logging",
+//            Items.STONE_AXE,
+//            new Vector4f(0.3f, 0.6f, 0.0f, 1.0f),
+//            (reqString) -> reqString.startsWith("lumberjack")
+//    ),
     FARMING(
             "Farming",
             Items.WHEAT_SEEDS,
             new Vector4f(0.8f, 0.6f, 0.2f, 1.0f),
             (reqString) -> reqString.startsWith("farm")
-    ),
-    PLANTING(
-            "Planting",
-            Items.OAK_SAPLING,
-            new Vector4f(0.2f, 0.8f, 0.2f, 1.0f),
-            (reqString) -> reqString.startsWith("forest")
     );
+//    PLANTING(
+//            "Planting",
+//            Items.OAK_SAPLING,
+//            new Vector4f(0.2f, 0.8f, 0.2f, 1.0f),
+//            (reqString) -> reqString.startsWith("forest")
+//    );
 
     private final String title;
     private final Item icon;
