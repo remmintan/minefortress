041a8606f604dac64628fbefc0924394af6eed50
remmintan <remmintan@gmail.com>
2023-02-14 23:15:46 +0100

    Fix join event to update profession manager after the server join

--- src/main/java/org/minefortress/professions/ServerProfessionManager.java
+++ src/main/java/org/minefortress/professions/ServerProfessionManager.java
@@ -187,7 +187,7 @@ public class ServerProfessionManager extends ProfessionManager{
         }
     }
 
-    private void scheduleSync() {
+    public void scheduleSync() {
         needsUpdate = true;
     }
 
