41bb07b4130ef23316af6ab429f3e8f606439373
remmintan <remmintan@gmail.com>
2023-05-04 14:16:30 +0200

    Remove oak planks from warriors

--- src/main/java/org/minefortress/entity/ai/MineFortressInventory.java
+++ src/main/java/org/minefortress/entity/ai/MineFortressInventory.java
@@ -7,15 +7,21 @@ import org.minefortress.registries.FortressItems;
 
 public class MineFortressInventory extends SimpleInventory {
 
-    public MineFortressInventory() {
+    public MineFortressInventory(boolean addPlanks) {
         super(36);
         super.setStack(0, ItemStack.EMPTY);
-        super.setStack(1, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
-        super.setStack(2, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
+        super.setStack(1, new ItemStack(Items.WATER_BUCKET));
+        super.setStack(2, new ItemStack(Items.BUCKET));
+        if(!addPlanks) return;
         super.setStack(3, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
-        super.setStack(4, new ItemStack(Items.WATER_BUCKET));
+        super.setStack(4, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
         super.setStack(5, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
-        super.setStack(6, new ItemStack(Items.BUCKET));
+        super.setStack(6, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
+        super.setStack(7, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
+        super.setStack(8, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
+        super.setStack(9, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
+        super.setStack(10, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
+        super.setStack(11, new ItemStack(FortressItems.SCAFFOLD_OAK_PLANKS, 64));
     }
 
 }
