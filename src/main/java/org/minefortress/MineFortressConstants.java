c4d8e3a88a48866a15a3e59b2aef96287e028722
remmintan <remmintan@gmail.com>
2023-06-04 10:54:00 +0200

    Made reach distance two times bigger thn it was before. Now it 120 blocks. Makes it easier to control your fortress

--- src/main/java/org/minefortress/MineFortressConstants.java
+++ src/main/java/org/minefortress/MineFortressConstants.java
@@ -2,7 +2,7 @@ package org.minefortress;
 
 public class MineFortressConstants {
 
-    public static final double PICK_DISTANCE = 60;
+    public static final double PICK_DISTANCE = 120;
     public static final float PICK_DISTANCE_FLOAT = (float) PICK_DISTANCE;
 
 }
