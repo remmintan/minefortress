f189e83148a23a6c6319cca80e2a9293f04e4e84
remmintan <remmintan@gmail.com>
2023-06-19 15:38:06 +0200

    Change the discord notification script

--- automation-scripts/discord-notification.sh
+++ automation-scripts/discord-notification.sh
@@ -3,7 +3,7 @@
 WEBHOOK_URL="$1"  # Replace with your webhook URL
 
 build_number="$2"  # Replace with the parameter for build number
-commit_messages="$3"  # Replace with the parameter for commit messages
+commit_messages=$3  # Replace with the parameter for commit messages
 role_id="$4"  # Replace with the parameter for role ID
 
 message_body="New dev build available!
@@ -16,6 +16,17 @@ https://builds.minefortress.org/
 
 <@${role_id}>"
 
-json_payload="{\"content\": \"$message_body\"}"
+escaped_message_body=${message_body//$'\n'/\\n}
 
-curl -X POST -H "Content-Type: application/json" -d "$json_payload" "$WEBHOOK_URL"
+json_payload=$(
+  cat <<EOF
+  {
+    "content": "${escaped_message_body}"
+  }
+EOF
+)
+
+echo "Sending message to Discord..."
+curl -H "Content-Type: application/json" \
+     -d "$json_payload" "$WEBHOOK_URL"
+echo "Message sent!"
