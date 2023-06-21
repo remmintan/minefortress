52ee0b907559073d173b3eafd208e13418a1b0b9
remmintan <remmintan@gmail.com>
2023-06-21 15:37:41 +0200

    Add reddit notification change the message

--- automation-scripts/discord-notification.sh
+++ automation-scripts/discord-notification.sh
@@ -6,10 +6,9 @@ build_number="$2"  # Replace with the parameter for build number
 commit_messages=$3  # Replace with the parameter for commit messages
 role_id="$4"  # Replace with the parameter for role ID
 
-message_body="New dev build available!
-Build number: ${build_number}
+message_body="MineFortress Dev Build ${build_number} built successfully!
 
-What's new:
+Changes:
 ${commit_messages}
 
 https://builds.minefortress.org/
