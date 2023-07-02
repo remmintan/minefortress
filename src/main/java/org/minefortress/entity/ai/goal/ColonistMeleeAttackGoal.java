88243012f6b143a275baed5d54392c580e238f38
remmintan <remmintan@gmail.com>
2023-07-02 18:07:49 +0200

    Fix pawn melee attack range, now pawns will attack monsters after reaching them. before then were just standing near them

--- src/main/java/org/minefortress/entity/ai/goal/ColonistMeleeAttackGoal.java
+++ src/main/java/org/minefortress/entity/ai/goal/ColonistMeleeAttackGoal.java
@@ -19,6 +19,6 @@ public class ColonistMeleeAttackGoal extends MeleeAttackGoal {
                 Optional.ofNullable(colonist.getTarget())
                         .map(it -> it.squaredDistanceTo(colonist))
                         .orElse(Double.MAX_VALUE)
-                     <= 5f;
+                     <= Colonist.WORK_REACH_DISTANCE * Colonist.WORK_REACH_DISTANCE;
     }
 }
