58d290a47f46a16f0cbf6103c648a041ec928b6f
remmintan <remmintan@gmail.com>
2023-11-09 08:32:35 +0100

    Fix hire pawns screen textures

--- src/main/java/org/minefortress/renderer/gui/widget/ProgressArrowWidget.java
+++ src/main/java/org/minefortress/renderer/gui/widget/ProgressArrowWidget.java
@@ -10,6 +10,7 @@ import java.util.function.Supplier;
 public class ProgressArrowWidget implements Drawable, Element {
 
     private static final Identifier PROGRESS_TEXTURE = new Identifier("textures/gui/container/blast_furnace.png");
+    private static final Identifier BURN_PROGRESS_TEXTURE = new Identifier("container/furnace/burn_progress");
 
     private final Supplier<Integer> progressSupplier;
     private final int x;
@@ -27,7 +28,7 @@ public class ProgressArrowWidget implements Drawable, Element {
         int rawProgress = progressSupplier.get();
         int progress = (int) (rawProgress * 0.22);
         if (progress > 0) {
-            drawContext.drawTexture(PROGRESS_TEXTURE, x-11, y+2, 177, 14, progress + 1, 16, 256, 256);
+            drawContext.drawGuiTexture(BURN_PROGRESS_TEXTURE, 24, 16, 0, 0, x-11, y+2, progress+2, 16);
         }
     }
 
