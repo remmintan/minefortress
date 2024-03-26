973d59b533d9a5ece256dcd1c4a4d73c1b7c1148
remmintan <remmintan@gmail.com>
2024-03-26 13:37:27 +0100

    Prevent click event firing when closing some in game screen

--- src/main/java/org/minefortress/controls/MouseEvents.java
+++ src/main/java/org/minefortress/controls/MouseEvents.java
@@ -6,8 +6,8 @@ import net.remmintan.mods.minefortress.core.utils.CoreModUtils;
 import org.minefortress.utils.ModUtils;
 
 public class MouseEvents {
-
     private static boolean mousePressedInpreviousTick = false;
+    private static long lastCheckTime = 0;
 
     public static void checkMouseStateAndFireEvents() {
         final var mouse = MinecraftClient.getInstance().mouse;
@@ -15,18 +15,23 @@ public class MouseEvents {
         // mouse x and y coordinates
         final var mouseX = mouse.getX();
         final var mouseY = mouse.getY();
-        if(isMousePressed) {
-            if(!mousePressedInpreviousTick) {
-                if(ModUtils.getFortressClient().get_FortressHud().isHovered() || MinecraftClient.getInstance().currentScreen != null)
-                    return;
-                firePressEvent(mouseX, mouseY);
-            }
-        } else {
-            if(mousePressedInpreviousTick) {
-                fireReleaseEvent(mouseX, mouseY);
+
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastCheckTime <= 300) {
+            if (isMousePressed) {
+                if (!mousePressedInpreviousTick) {
+                    if (ModUtils.getFortressClient().get_FortressHud().isHovered() || MinecraftClient.getInstance().currentScreen != null)
+                        return;
+                    firePressEvent(mouseX, mouseY);
+                }
+            } else {
+                if (mousePressedInpreviousTick) {
+                    fireReleaseEvent(mouseX, mouseY);
+                }
             }
         }
         mousePressedInpreviousTick = isMousePressed;
+        lastCheckTime = currentTime;
     }
 
     private static void firePressEvent(double mouseX, double mouseY) {
@@ -35,8 +40,9 @@ public class MouseEvents {
         final var correctState = state == FortressState.COMBAT || state == FortressState.BUILD_SELECTION;
         final var noBuildingHovered = ModUtils.getFortressClientManager().getHoveredBuilding().isEmpty();
         final var noScreenOpened = MinecraftClient.getInstance().currentScreen == null;
+        final var hudNotHovered = !ModUtils.getFortressClient().get_FortressHud().isHovered();
 
-        if(correctState && noBuildingHovered && noScreenOpened) {
+        if (correctState && noBuildingHovered && noScreenOpened && hudNotHovered) {
             final var provider = CoreModUtils.getMineFortressManagersProvider();
             final var pawnsSelection = provider.get_PawnsSelectionManager();
             pawnsSelection.startSelection(mouseX, mouseY);
@@ -48,5 +54,4 @@ public class MouseEvents {
         final var pawnsSelection = provider.get_PawnsSelectionManager();
         pawnsSelection.endSelection(mouseX, mouseY);
     }
-
 }
