105681f6b66b48a1f5eb2c3002ce8b0e687037a0
remmintan <remmintan@gmail.com>
2024-10-11 22:58:25 +0200

    Labeled more blueprints with the corresponding professions

--- src/core/java/net/remmintan/mods/minefortress/core/interfaces/blueprints/BlueprintRequirementType.kt
+++ src/core/java/net/remmintan/mods/minefortress/core/interfaces/blueprints/BlueprintRequirementType.kt
@@ -5,9 +5,14 @@ import net.minecraft.item.Items
 
 enum class BlueprintRequirementType(val icon: Item, val displayName: String, val requirementsIds: List<String>) {
 
-    FARMER(Items.WHEAT, "Farmer", listOf("farmer")),
+    FARMER(Items.WHEAT, "Farmer", listOf("farmer", "farmer2")),
     MINER(Items.STONE_PICKAXE, "Miner", listOf("miner_wooden", "miner_stone", "miners_guild")),
     LUMBERJACK(Items.STONE_AXE, "Lumberjack", listOf("lumberjack_wooden", "lumberjack_stone", "lumberjack_guild")),
-    BLACKSMITH(Items.FURNACE, "Blacksmith", listOf("blacksmith")),
+    BLACKSMITH(Items.FURNACE, "Blacksmith", listOf("blacksmith", "blacksmith2")),
+    FORESTER(Items.APPLE, "Forester", listOf("forester")),
+    WARRIOR(Items.STONE_SWORD, "Warrior", listOf("warrior1", "warrior2")),
+    ARCHER(Items.BOW, "Archer", listOf("shooting_gallery")),
+    FISHERMAN(Items.FISHING_ROD, "Fisher", listOf("fisher")),
+    CRAFTSMAN(Items.CRAFTING_TABLE, "Craftsman", listOf("crafter"))
 
 }
